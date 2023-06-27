f33747e632780e2e28bfb4ed2ee22ea88a7220bb
ddqlpajama <ddqlpajama@gmail.com>
2023-06-26 21:39:14 -0400

    - Updated npm - Removed unnecessary counter - Implemented LWA SDK call

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,19 +2,24 @@
 	import { onMount } from 'svelte';
 	import Header from './Header.svelte';
 	import './styles.css';
+	import { PUBLIC_LWA_CLIENT_ID } from '$env/static/public';
 
 	let amazonroot: HTMLDivElement;
-	let onAmazonLoginReady: () => void;
 
-	onMount(() => {
+	onMount(() => {		
 		var a = document.createElement('script');
+		a.type = 'text/javascript';
+		a.innerHTML = `window.onAmazonLoginReady = function() {
+			amazon.Login.setClientId("${PUBLIC_LWA_CLIENT_ID}"); 
+		};`;
+		amazonroot.appendChild(a);
+
+		a = document.createElement('script');
 		a.type = 'text/javascript';
 		a.async = true;
 		a.id = 'amazon-login-sdk';
 		a.src = 'https://assets.loginwithamazon.com/sdk/na/login1.js';
 		amazonroot.appendChild(a);
-
-		onAmazonLoginReady = 
 	});
 </script>
 
